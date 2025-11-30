a47795b8c3f4e9fcfc78a138ba5f0b3e9f11664d
kaxcheg <kaxcheg@gmail.com>
2025-11-30 11:38:09 +0100

    linting fixed

--- app/domain/entities/user/repo.py
+++ app/domain/entities/user/repo.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from  abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 
 from app.domain.entities.base import Repository
 from app.domain.entities.user import User
